8fc77e3818de78a435a2289732d3ab8a301d9ab6
Mike Schinkel <mikeschinkel@gmail.com>
2016-04-26 00:47:25 -0400

    Renamed some setup files.

--- setup/030-setup-cloud-files.sh
+++ setup/030-setup-cloud-files.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
-echo "Setting Google DNS..."
-sudo networksetup -setdnsservers "Wi-Fi" "8.8.8.8" "8.8.4.4"
+echo "Setting Cloud Files..."
+echo
+
+mkdir -p "~/Cloud Files"
+cd "~/Cloud Files"
+
+mkdir -p "OneDrive"
+mkdir -p "Google Drive"
+mkdir -p "Box Sync"
+mkdir -p "DropBox (NewClarity)"
+
+
+ln -s  "${HOME}/OneDrive" "${HOME}/Cloud Files/OneDrive"
+ln -s  "${HOME}/Google Drive" "${HOME}/Cloud Files/GDrive"
+ln -s  "${HOME}/Box Sync" "${HOME}/Cloud Files/Box.com"
+ln -s  "${HOME}/DropBox (NewClarity)" "${HOME}/Cloud Files/DropBox"
 echo
