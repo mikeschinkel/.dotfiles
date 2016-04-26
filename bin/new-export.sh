1e96f018da82056a0d8489c7e195af763aad62fc
Mike Schinkel <mikeschinkel@gmail.com>
2016-04-26 00:45:36 -0400

    Removed the .sh extension from /bin scripts

--- bin/new-export.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-EXPORT_FILE=$(to-lower.sh "$1")
-new-script.sh exports/${EXPORT_FILE} "echo \"$2\""
-export-var.sh ${EXPORT_FILE}
--- bin/personal-email.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-get-config.sh ".email.personal"
