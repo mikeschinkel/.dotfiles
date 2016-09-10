bd22913144a6ff19ea4981834ab954bf5388b5af
The WPLib Team <wplib@Thes-MacBook-Pro.local>
2016-09-10 03:21:42 -0400

    Lastest Commit

--- install/100-install-sublime-text.sh
+++ install/100-install-sublime-text.sh
@@ -4,6 +4,7 @@ echo Installing Sublime Text...
 brew cask install sublime-text
 
 echo Setting Preferences Files...
-$sublime_dir=$(ls "~/Library/Application Support/" | grep Sublime)
-sudo cp "../config/Preferences.sublime-settings" "~/Library/Application Support/${sublime_dir}/Packages/User/Preferences.sublime-settings"
+sublime_dir=$(ls ~/Library/Application\ Support/ | grep Sublime)
+
+sudo cp "${DOTFILES_DIR}/config/Preferences.sublime-settings" "~/Library/Application Support/${sublime_dir}/Packages/User"
 
