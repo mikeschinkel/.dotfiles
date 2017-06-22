2f8bd728da4e8da7177a38caefd91a9a4d89e857
Mike Schinkel <mike@newclarity.net>
2017-06-22 01:59:35 -0400

    Updated and improved ~/.dotfiles/install/100-install-golang.sh

---
+++ exports/gopath.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo "/Users/mschinkel/GoLang"
--- install/100-install-golang.sh
+++ install/100-install-golang.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
-echo Installing Go Language...
-brew isntall golang
+echo "Installing Go Language..."
+brew install golang
+
+chmod +x "${DOTFILES_DIR}/bin/new-export"
+new-export "GOPATH" "${HOME}/GoLang"
+new-export "GOROOT" "/usr/local/opt/go/libexec"
+
+GOPATH="${HOME}/GoLang"
+GOROOT="/usr/local/opt/go/libexec"
+
+chmod +x "${DOTFILES_DIR}/bin/new-path"
+new-path GOLANG "${GOPATH}/bin"
+new-path GOROOT "${GOROOT}/bin"
