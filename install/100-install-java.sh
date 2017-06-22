211409a8901e186604cfdb2c93b8d74e0918d35a
Mike Schinkel <mike@newclarity.net>
2017-06-22 02:08:58 -0400

    Fucking Oracle

--- install/100-install-java.sh
+++ install/100-install-java.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
-echo Installing Java...
-wget http://support.apple.com/downloads/DL1572/en_US/javaforosx.dmg
+echo "Fucking download from here, fucking Oracle that won't make it so I can script it..."
+echo
+echo "   https://www.java.com/en/download/manual.jsp"
+echo
 
-echo Mounting DMG...
-hdiutil attach javaforosx.dmg
+#echo "Installing Java..."
+#wget http://support.apple.com/downloads/DL1572/en_US/javaforosx.dmg
 
-echo Installing Java for OSX...
-PACKAGE=$(ls /Volumes | grep "Java")
-sudo installer -package "/Volumes/${PACKAGE}/JavaForOSX.pkg" -target /
+#echo "Mounting DMG..."
+#hdiutil attach javaforosx.dmg
 
-new-export JAVA_HOME "\$(/usr/libexec/java_home)"
+#echo Installing Java for OSX..."
+#PACKAGE=$(ls /Volumes | grep "Java")
+#sudo installer -package "/Volumes/${PACKAGE}/JavaForOSX.pkg" -target /
 
-echo Cleaning up...
-detach-dmg Java
+#new-export JAVA_HOME "\$(/usr/libexec/java_home)"
 
-rm javaforosx.dmg
+#echo Cleaning up...
+#detach-dmg Java
+
+#rm javaforosx.dmg
