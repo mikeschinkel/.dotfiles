d6c93c60b422f1def6467bdd13fa69b43ced6ff0
Mike Schinkel <mike@newclarity.net>
2018-09-12 21:09:06 -0400

    Improved prompt.

--- bash/010-prompt.sh
+++ bash/010-prompt.sh
@@ -1,88 +1,132 @@
 #!/usr/bin/env bash
 
-    #
-    # Display Git stuff in my prompt
-    #
-    # From http://slacy.com/blog/2012/06/supercharge-your-bash-prompt-with-git-status-goodness/
-    #
-    # TODO: Change repo to "{org}/{repo}" for origin (fetch)
-    #
-    #       git remote -v | grep "(fetch)" | grep "origin"
-    #
-    echo "Running .dotfiles/bash/010-prompt.sh..."
+#
+# Display Git stuff in my prompt
+#
+# From http://slacy.com/blog/2012/06/supercharge-your-bash-prompt-with-git-status-goodness/
+#
+# TODO: Change repo to "{org}/{repo}" for origin (fetch)
+#
+#       git remote -v | grep "(fetch)" | grep "origin"
+#
+echo "Running .dotfiles/bash/010-prompt.sh..."
 
-    function dev_prompt {
-        local reset='\[\e[00m\]'
-        local bold='\[\e[01m\]'
-        local red='\[\e[31m\]'
-        local green='\[\e[32m\]'
-        local yellow='\[\e[33m\]'
-        local blue='\[\e[34m\]'
-        local purple='\[\e[35m\]'
-        local cyan='\[\e[36m\]'
-        local gray='\[\e[37m\]'
-        local bright='\[\e[39m\]'
-        local bc="${bold}${cyan}"
-        local bg="${bold}${green}"
-        local by="${bold}${yellow}"
-        local br="${bold}${red}"
-        local r="${reset}"
-        local git=""
-        local dir
-
-        if [ "$PWD" == "/" ]; then
-            dir="/"
-        else
-            dir="\w/"
+function _get_project_json() {
+    local dir="$(pwd)"
+    until [ "/" == "${dir}" ] ; do
+        if [ -f "${dir}/project.json" ] ; then
+            echo "$(cat "${dir}/project.json")"
+            break
         fi
+        dir="$(dirname "${dir}")"
+    done
+}
 
-        if [ -d "$(pwd)/.git" ]; then
-            local branch="$(git branch --no-color | grep "*" | cut -c3-999 2>&1)"
-            local hash="$(git log -1 --oneline --no-color|awk '{print $1}' 2>&1)"
+function _get_git_dir() {
+    local dir="$(pwd)"
+    until [ "/" == "${dir}" ] ; do
+        if [ -d "${dir}/.git" ] ; then
+            echo "${dir}/.git"
+            break
+        fi
+        dir="$(dirname "${dir}")"
+    done
+}
 
-            local tag="$(git describe --exact-match "${hash}" 2>&1)"
-            if ! [[ "" == "${tag}" || "${tag}" =~ ^fatal ]]; then
-                git="${br}tag${r}=${bg}${tag}${r}"
-            fi
+function dev_prompt {
+    local reset='\[\e[00m\]'
+    local bold='\[\e[01m\]'
+    local red='\[\e[31m\]'
+    local green='\[\e[32m\]'
+    local yellow='\[\e[33m\]'
+    local blue='\[\e[34m\]'
+    local purple='\[\e[35m\]'
+    local cyan='\[\e[36m\]'
+    local gray='\[\e[37m\]'
+    local bright='\[\e[39m\]'
+    local bc="${bold}${cyan}"
+    local bg="${bold}${green}"
+    local by="${bold}${yellow}"
+    local br="${bold}${red}"
+    local bb="${bold}${blue}"
+    local r="${reset}"
+    local git=""
+    local dir
 
-            if [ "(HEAD detached at" == "${branch:0:17}" ]; then
-                if [ "" != "${git}" ]; then
-                    git=", ${git}"
-                fi
-                git="${br}HEAD${reset}=${bg}${branch:18:8}${r} (detached)${git}"
-            elif [ "" != "${branch}" ] ; then
-                git="${git}${br}branch${reset}=${bg}${branch}${r}"
-            else
-                git=""
-            fi
+    if [ "$PWD" == "/" ]; then
+        dir="/"
+    else
+        dir="\w/"
+    fi
+    git_dir="$(_get_git_dir)"
+    if [ -d "${git_dir}" ]; then
 
-            local remote="$(git remote 2>&1)"
-            if ! [[ "" == "${remote}" || "${remote}" =~ ^fatal ]]; then
-                if [ "" != "${git}" ]; then
-                    git="${git}, "
-                fi
-                git="${git}${br}remote${r}=${bg}${remote}${r}"
-            fi
+        pushd "${git_dir}" > /dev/null
 
+        local branch="$(git branch --no-color | grep "*" | cut -c3-999 2>&1)"
+        local hash="$(git log -1 --oneline --no-color|awk '{print $1}' 2>&1)"
+        local tag="$(git describe --exact-match "${hash}" 2>&1)"
+
+        if ! [[ "" == "${tag}" || "${tag}" =~ ^fatal ]]; then
+            git="${br}tag${r}=${bg}${tag}${r} (detached)"
+        elif [ "(HEAD detached at" == "${branch:0:17}" ]; then
             if [ "" != "${git}" ]; then
-                git="${by}GIT:${r} ${git}\n"
+                git=", ${git}"
             fi
+            git="${br}HEAD${reset}=${bg}${hash}${r} (detached)${git}"
+        elif [ "" != "${branch}" ] ; then
+            git="${git}${br}branch${reset}=${bg}${branch}${r}"
+        else
+            git=""
         fi
 
-        dir="${bc}${dir} \n"
+        local remote="$(git config --get "branch.${branch}.remote")"
+        if ! [[ "" == "${remote}" || "${remote}" =~ ^fatal ]]; then
+            if [ "" != "${git}" ]; then
+                git="${git}, "
+            fi
+            git="${git}${br}remote${r}=${bg}${remote}${r}"
+        fi
 
-        PS1="\n${git}${dir}${reset}${green}\$${reset} "
+        local repo="$(basename "$(git config --get "remote.${remote}.url")")"
+        if [ "" != "${repo}" ]; then
+            if [ "" != "${git}" ]; then
+                git=", ${git}"
+            fi
+            git="${br}repo${r}=${bg}${repo}${r}${git}"
+        fi
 
-    }
+        if [ "" != "${git}" ]; then
+            git="${bb}GIT:${r}     ${git}"
+        fi
 
-    PROMPT_COMMAND=dev_prompt
+        popd > /dev/null
 
+    fi
 
-      # httpd="${purple}apache[${reset}${cyan}"
-      # running=$(ps ax | grep -v grep | grep httpd)
-      # if  [ "${running}" == "" ]; then
-      #     httpd="${httpd}off"
-      # else
-      #     httpd="${httpd}ON"
-      # fi
-      # httpd="${httpd}${reset}${purple}]${reset}"
+    dir="${bc}${dir} \n"
+
+    PS1="\n${git}${dir}${reset}${green}\$${reset} "
+
+    json="$(_get_project_json)"
+    if [ "" == "${json}" ]; then
+        project=""
+    else
+        project="$(echo $json|jq -r '.hostname')"
+    fi
+
+    if [ "" != "${project}" ]; then
+        project="${bb}Project:${r} ${br}host${r}=${bg}${project}${r}"
+    fi
+
+    if [[ "" != "${project}" && "" != "${git}" ]]; then
+        project="\n${project}"
+    fi
+
+    dir="${bc}${dir}"
+
+    PS1="\n${by}WPLib Box${bg}!${r}\n${git}${project}\n${dir} \n${reset}${green}\$${reset} "
+
+}
+
+PROMPT_COMMAND=dev_prompt
