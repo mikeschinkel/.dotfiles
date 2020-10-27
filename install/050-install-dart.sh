a72ae2d5a2fabc8f9534fdd79077f00c3b160d9f
Mike Schinkel <mike@newclarity.net>
2020-10-26 20:08:13 -0400

    Added Dart install

---
+++ install/050-install-dart.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# See https://v1-dartlang-org.firebaseapp.com/install/mac
+
+
+echo Installing Dart...
+brew tap dart-lang/dart
+brew install dart
+brew tap dart-lang/dart
+brew install dart --with-content-shell --with-dartium
